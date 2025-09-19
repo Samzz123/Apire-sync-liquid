02dac2091799c66a59a518e43dd622225572d8b0
Samzz123 <138846276+Samzz123@users.noreply.github.com>
2025-09-19 21:11:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-<<<<<<< HEAD
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': 'http://localhost:3000' // vercel dev runs on 3000
-    }
-  }
-=======
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
->>>>>>> fed4b15ae02a1fa3d5418c4b433ddc9930172977
-})
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+  },
+});
